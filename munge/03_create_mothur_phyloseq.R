ac21b08bb8df94a08b2c96531e6c7d289e468679
Nate Olson <nolson@nist.gov>
2018-01-16 11:56:24 -0500

    added comments and revised code

--- munge/03_create_mothur_phyloseq.R
+++ munge/03_create_mothur_phyloseq.R
@@ -2,57 +2,73 @@
 require(ape)
 require(Biostrings)
 
-## Mothur files
-shared_file <- "data/mothur/mgtst.trim.contigs.good.unique.good.filter.unique.precluster.pick.opti_mcc.unique_list.shared"
+############################## Define Mothur Files #############################
+## Defines file names and checks if the files exists  
 
-rep_fasta <- "data/mothur/mgtst.trim.contigs.good.unique.good.filter.unique.precluster.pick.opti_mcc.unique_list.0.03.rep.fasta"
+file_basename <- paste0("mgtst.trim.contigs.good.unique.good.filter.",
+                        "unique.precluster.pick.opti_mcc.unique_list.")
+file_basename <- file.path("data", "mothur", file_basename)
 
-constax_file <- "data/mothur/mgtst.trim.contigs.good.unique.good.filter.unique.precluster.pick.opti_mcc.unique_list.0.03.cons.taxonomy"
+shared_file <- paste0(file_basename, "shared")
+if (!file.exists(shared_file)) stop("File not found - ", shared_file)
 
-tree_file <- "data/mothur/mgtst.trim.contigs.good.unique.good.filter.unique.precluster.pick.opti_mcc.unique_list.0.03.rep.phylip.tre"
+rep_fasta <- paste0(file_basename, "0.03.rep.fasta")
+if (!file.exists(rep_fasta)) stop("File not found - ", rep_fasta)
 
-seq_file <- "data/mothur/mgtst.trim.contigs.good.unique.good.filter.unique.precluster.pick.opti_mcc.unique_list.0.03.rep.ng.fasta"
+constax_file <- paste0(file_basename, "0.03.cons.taxonomy")
+if (!file.exists(constax_file)) stop("File not found - ", constax_file)
 
-## Replacing rep seq name with OTU id 
-otu_id_dat <- read_lines(rep_fasta)
-otu_id_dat <- otu_id_dat[grepl("^>",x = otu_id_dat)] %>% 
+tree_file <- paste0(file_basename, "0.03.rep.phylip.tre")
+if (!file.exists(tree_file)) stop("File not found - ", tree_file)
+
+seq_file <- paste0(file_basename, "0.03.rep.ng.fasta")
+if (!file.exists(seq_file)) stop("File not found - ", seq_file)
+
+
+
+
+######################### Renaming OTUs ######################################## 
+#  ids for seq and tree files do not match shared and tax file
+otu_id_dat <- read_lines(rep_fasta) %>% 
+    ## Only need read names
+    {.[grepl("^>",x = .)]} %>% 
+    ## Excluding information after first '|'
     str_replace("\\|.*", "")
+    
 
+## Generating a data frame with the representative sequence ids and otu ids 
 otu_id_df <- data_frame(ids = otu_id_dat) %>% 
     separate(ids, c("seq_id","otu_id"), "\t") %>% 
     mutate(seq_id = str_replace(seq_id, ">",""))
 
-## Tree data
+## Tree data -------------------------------------------------------------------
 tree_dat <- read.tree(tree_file)
-
-### Check for correct label matching
-## label_test <- data_frame(seq_id = tree_dat$tip.label)
-
 tree_dat$tip.label <- otu_id_df$otu_id[match(tree_dat$tip.label, otu_id_df$seq_id)]
 
-## label_test$otu_id <- tree_dat$tip.label
-## nrow(otu_id_df) == nrow(label_test)
-##nrow(otu_id_df) == nrow(left_join(otu_id_df, label_test))
-
-## Seq data
+## Seq data --------------------------------------------------------------------
 seq_dat <- readDNAStringSet(seq_file)
 names(seq_dat) <- otu_id_df$otu_id[match(names(seq_dat), otu_id_df$seq_id)]
 
-### Generate phyloseq object
+
+
+
+################### Generate Phyloseq Object ###################################
 mothur_ps <- import_mothur(mothur_shared_file = shared_file,
                            mothur_constaxonomy_file = constax_file)
 
-## Adding sample metadata
+## Adding sample metadata ------------------------------------------------------
 ## Modifying sample names for consistency
 sample_names(mothur_ps) <- sample_names(mothur_ps) %>% 
     str_replace("(?<=[:digit:])_(?=[:LETTER:])","-")
 
+## Defining metadata slot ------------------------------------------------------
 sample_data(mothur_ps) <- column_to_rownames(mgtstMetadata, var = "sample_id")
 
-## Adding Tree Data
+## Defining tree slot ----------------------------------------------------------
 phy_tree(mothur_ps) <- tree_dat
 
-## Adding seq data 
+## Defining refseq slot --------------------------------------------------------
 mothur_ps@refseq <- seq_dat
 
+###################### Caching Phyloseq Object #################################
 ProjectTemplate::cache("mothur_ps")
