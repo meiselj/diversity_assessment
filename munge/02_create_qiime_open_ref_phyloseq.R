34bd542704e77cf887367ed55e35b972476b71dc
Nate Olson <nolson@nist.gov>
2018-01-17 22:04:40 -0500

    updated for open ref results

--- munge/02_create_qiime_open_ref_phyloseq.R
+++ munge/02_create_qiime_open_ref_phyloseq.R
@@ -3,24 +3,29 @@ require(Biostrings)
 require(ape)
 
 #### QIIME Phyloseq Object
-qiime_dir <- "data/qiime_open_ref"
-biom_file <- file.path(qiime_dir, "otu_table_mc2_w_tax_no_pynast_failures.biom")
+
+## Issue loading biom file convert to json using biom convert 
+## ```
+## biom convert -i otu_table_mc2_w_tax_no_pynast_failures.biom \
+##              -o otu_table_mc2_w_tax_no_pynast_failures_json.biom \
+##              --table-type "OTU table" --to-json
+## ```
+qiime_dir <- "data/qiime_open_ref" 
+biom_file <- file.path(qiime_dir, "otu_table_mc2_w_tax_no_pynast_failures_json.biom")
 seq_file <- file.path(qiime_dir, "rep_set.fna")
 tree_file <- file.path(qiime_dir, "rep_set.tre")
 
 qiime_open_ref_ps <- import_biom(BIOMfilename = biom_file)
 
+## Update sample names to match metadata formated sample names
+sample_names(qiime_open_ref_ps) <- sample_names(qiime_open_ref_ps) %>%  
+    str_replace("^N","nist_run") %>% 
+    str_replace("^J", "jhu_run") %>% 
+    str_replace("(?<=[:digit:])-(?=[:digit:])","_")
+
 
 ## Adding sample metadata
-# sample_data(qiime_open_ref_ps) <- column_to_rownames(mgtstMetadata, var = "sample_id")
-
-################ Renaming OTUs #################################################
-##
-## Addresses issue with OTU names interpreted as numeric and character strings 
-##
-
-# taxa_names(qiime_open_ref_ps) <- paste0("OTU_", taxa_names(qiime_open_ref_ps))
-biom_otus <- taxa_names(qiime_open_ref_ps)
+sample_data(qiime_open_ref_ps) <- column_to_rownames(mgtstMetadata, var = "sample_id")
 
 ################ Adding seq data to phyloseq object ############################
 
@@ -28,10 +33,10 @@ seq_dat <- readDNAStringSet(seq_file)
 
 ###### Reformating OTU names
 names(seq_dat) <- names(seq_dat) %>%
-    str_replace(" .*","") #%>%
-    # {paste0("OTU_",.)}
+    str_replace(" .*","")
 
 ###### Filtering rep seq set
+biom_otus <- taxa_names(qiime_open_ref_ps)
 filtered_seq_dat <- seq_dat[names(seq_dat) %in% biom_otus]
 
 ## Defining seq slot
@@ -40,11 +45,6 @@ qiime_open_ref_ps@refseq <- filtered_seq_dat
 ################ Adding tree data to phyloseq object ###########################
 
 tree_dat <- read.tree(tree_file)
-
-###### Reformating OTU names
-# tree_dat$tip.label <- paste0("OTU_", tree_dat$tip.label)
-
-# tree_dat$tip.label
 
 ## Defining tree slot
 phy_tree(qiime_open_ref_ps) <- tree_dat
---
+++ munge/08_beta_eval_comparison_to_expectation.R
@@ -0,0 +1,131 @@
+## PAM base cluster evaluation similar to normalization method assessment for
+## beta diversity metrics used in McMurdie and Holmes 2014 and Weiss et al 2017
+## comparing unmixed pre-exposure samples to titration and post-exposure samples
+
+
+##################### Functions for PAM Cluster Evaluation #####################
+
+#' Bicluster samples using PAM
+#'
+#' @param comp_df data frame with samples in sample_ids column
+#' @param dist_mat matrix with pairwise distances
+#'
+#' @return named vector with cluster assignments
+#' @export
+#'
+#' @examples
+cluster_samples <- function(comp_df, dist_mat){
+    comp_mat <- dist_mat[row.names(dist_mat) %in% comp_df$sample_id,
+                         colnames(dist_mat) %in% comp_df$sample_id]
+    if (nrow(comp_mat) <= 2) {
+        return(NULL)
+    }
+    cluster::pam(comp_mat, k = 2, cluster.only = TRUE)
+}
+
+#' Evaluate PAM clustering results
+#'
+#' @param cluster_assignments named vector with cluster assignments
+#' @param comp_df data frame with sample_ids (samples) and t_fctr column (grouping)
+#'
+#' @return double with percent of matched samples
+#' @export
+#'
+#' @examples
+cluster_eval <- function(cluster_assignments, comp_df){
+    if (nrow(comp_df) != length(cluster_assignments)) {
+        comp_df <- filter(comp_df, sample_id %in% names(cluster_assignments))
+    }
+    
+    if (is.null(cluster_assignments)) {
+        return(NA)
+    }
+    
+    ## Ensuring sample_ids are in the correct order
+    results_df <- data.frame(cluster_results = cluster_assignments) %>% 
+        rownames_to_column(var = "sample_id") %>% 
+        left_join(comp_df)
+    
+    ## Number of samples clustered
+    samples_compared <- length(cluster_assignments)
+    
+    ## Evaluate comparison for either possible cluster assignment
+    cluster_eval <- ifelse(results_df$t_fctr == min(results_df$t_fctr), 1, 2)
+    cluster_eval_inv <- ifelse(results_df$t_fctr == max(results_df$t_fctr), 1, 2)
+    
+    assignment_matches <- max(sum(cluster_eval == results_df$cluster_results), 
+                              sum(cluster_eval_inv == results_df$cluster_results))  
+    
+    ## Return the fraction of samples correctly clustered
+    assignment_matches/samples_compared
+}
+
+#' Perform PAM cluster evaluation
+#'
+#' @param dist_obj dist-class object with pairwise distances
+#' @param full_comp_df data frame with full set of comparisons
+#'
+#' @return data frame with cluster evaluation results
+#' @export
+#'
+#' @examples
+get_cluster_eval_df <- function(dist_obj, full_comp_df){
+    dist_mat <- as.matrix(dist_obj) 
+    
+    full_comp_df %>%
+        mutate(cluster_assignments = map(comp_df, cluster_samples, dist_mat)) %>%
+        mutate(cluster_results = map2_dbl(cluster_assignments, comp_df, cluster_eval))
+}
+
+######################## Generating Comparison data frame ######################
+condensed_meta <- mgtstMetadata %>% 
+    filter(biosample_id != "NTC") %>% 
+    mutate(sample_id = as.character(sample_id)) %>% 
+    group_by(seq_lab, seq_run, biosample_id) %>% 
+    select(-pcr_16S_plate, -pos) %>% 
+    nest()
+
+comparison_meta <- condensed_meta %>% 
+    select(-data) %>% 
+    add_column(t_comp = list(c(0:5,10,15))) %>% 
+    unnest() %>% 
+    left_join(condensed_meta) 
+
+full_comp_df <- comparison_meta %>% 
+    ## Generating data frame with sample_id and t_fctr for comparisons to perform
+    mutate(comp_df = map2(data, t_comp, ~filter(.x, t_fctr %in% c(.y,20 )))) %>% 
+    select(-data)
+
+######################## Unweighted Metric Evaluation ########################## 
+
+## Perform cluster evaluation 
+full_cluster_eval_df <- unweighted_beta_df %>% 
+    gather("metric","dist_output", -pipe, -method) %>% 
+    mutate(dist_obj = map(dist_output, pluck, "result"))
+    mutate(eval_results = map(dist_obj, get_cluster_eval_df, full_comp_df))
+
+## Tidy cluster evaluation results
+beta_cluster_eval_unweighted_df <- full_cluster_eval_df %>% 
+    select(-dist_obj, -dist_output) %>% 
+    unnest() %>% 
+    select(-comp_df, -cluster_assignments)  
+
+## Caching evaluation results  
+ProjectTemplate::cache("beta_cluster_eval_unweighted_df")
+
+######################## Weighted Metric Evaluation ########################## 
+
+## Perform cluster evaluation 
+full_cluster_eval_df <- weighted_beta_df %>% 
+    gather("metric","dist_output", -pipe, -method) %>% 
+    mutate(dist_obj = map(dist_output, pluck, "result"))
+    mutate(eval_results = map(dist_obj, get_cluster_eval_df, full_comp_df))
+
+## Tidy cluster evaluation results
+beta_cluster_eval_weighted_df <- full_cluster_eval_df %>% 
+    select(-dist_obj, -dist_output) %>% 
+    unnest() %>% 
+    select(-comp_df, -cluster_assignments)   
+
+## Caching evaluation results  
+ProjectTemplate::cache("beta_cluster_eval_weighted_df")
